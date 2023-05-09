66a1834d38042ba791de0fe4199dc309bc9ba673
lolacmartinlopez <lola.cristina.martin.lopez@edag-ps.com>
2023-05-09 09:00:34 +0200

    Home hecho

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,26 +1,50 @@
 import * as React from 'react';
-import fondoTienda from '../assets/bagLogo.png';
 import { Container } from '@mui/material';
 import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
-import Image from '@mui/icons-material';
+import handBagInitial from '../assets/handbag.png';
+import { makeStyles } from "@mui/styles";
+import {NavLink} from "react-router-dom";
+
+
+const useStyles = makeStyles((theme)=> ({
+  root: {
+     width: "100%",
+     height: "400px",
+     backgroundColor: '#fdd9f8',
+  },
+
+  image: {
+
+      textAlign: "center",
+      display: "block",
+      justifyContent: "center",
+      alignItems: "center",
+      margin: "auto",
+      width: "65%",
+
+  },
+
+   text: {
+    height: "10rem",
+    marginLeft: "200px",
+    fontFamily: 'Myriad Pro Regular',
+  },
+
+})); 
 
 export function HomePage() {
+  const classes = useStyles()
   return (
-        <Box
-          sx={{
-            width: "100%",
-            height: "400px",
-            backgroundColor: '#fdd9f8',
-            backgroundImage: '../assets/fondoTienda.jpg'
-          }}
-        >
+        <Box className= {classes.root}>
           <Container>
-            <IconButton position= "centered"
-            > Presione para continuar.</IconButton>
+          <NavLink to= "/products" exact>
+            <IconButton>
+              <img src = {handBagInitial} className={classes.image}/>
+            </IconButton>
+            <p className={classes.text}> Encontrar mi bolso </p>
+            </NavLink>
           </Container>
-
         </Box>
     )
   }
